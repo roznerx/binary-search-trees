d192d884a5b073cba32e8aff0446b59407128917
unknown <ignaciorosner@gmail.com>
2024-01-29 21:30:36 -0300

    Still working around the insert and delete methods

--- bts.js
+++ bts.js
@@ -9,25 +9,77 @@ class Node {
 class Tree {
   constructor(arr) {
     this.arr = arr;
-    this.root = this.buildTree();
+    this.root = this.buildTree(this.arr);
   };
 
-  buildTree() {
-    if (this.arr.length === 0) {
+  buildTree(data) {
+    let cleanData = [...new Set(data)].sort((a, b) => a - b);
+
+    if (cleanData.length === 0) {
       return null;
     };
     
     // Build the tree
-    let cleanArr = [...new Set(this.arr)].sort((a, b) => a - b);
-    let midIndex = Math.floor(cleanArr.length / 2);
-    let root = new Node(cleanArr[midIndex]);
-    
-    // Recursion would be nice to use
+    let midIndex = Math.floor(cleanData.length / 2);
+    let root = new Node(cleanData[midIndex]);
+
+    // Recursion to build left and right subtrees
+    root.leftChild = this.buildTree(cleanData.slice(0, midIndex));
+    root.rightChild = this.buildTree(cleanData.slice(midIndex + 1));
 
     // The buildTree function should return the level-0 root node.
     return root;
   };
+
+  insert(node, value) {
+    // value is a number
+    if (typeof value !== 'number') {
+      return "Value should be a number!";
+    };
+
+    // if value already exists on tree, return
+    if (this.arr.includes(value)) {
+      return;
+    } else {
+      // if value already exists on tree, return
+      if (value < node.data) {
+        if (node.leftChild === null) {
+          node.leftChild = new Node(value);
+        } else {
+          this.insert(node.leftChild, value);
+        };
+      } else if (value > node.data) {
+        if (node.rightChild === null) {
+          node.rightChild = new Node(value);
+        } else {
+          this.insert(node.rightChild, value);
+        };
+      };
+    };
+  };
+
+  delete(value) {}
+
+  // This function helps visualize the tree, provided by The Odin Project
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    };
+
+    if (node.rightChild !== null) {
+      this.prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    };
+
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+
+    if (node.leftChild !== null) {
+      this.prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    };
+  };
+
 };
 
 let myTree = new Tree([1, 7, 4, 6345, 23, 8, 1, 9, 4, 32344, 3, 5, 7, 9, 67, 324]);
-console.log(myTree.buildTree());
+myTree.insert(myTree.root, 2);
+myTree.prettyPrint(myTree.root);
+console.log(myTree.root);
